52284e1ce9c5d1f58c3f0896908aafc9f248d92d
namte24 <tenam2000@hotmail.com>
2023-04-11 11:37:43 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import "./App.css";
+//import UilReact from "@iconscout/react-unicons/icons/uil-react";
+import TopButtons from "./Components/TopButtons";
+import Inputs from "./Components/Inputs";
+import TimeAndLocation from "./Components/TimeAndLocation";
+import Details from "./Components/Details";
+import Forecast from "./Components/Forecast";
+import getFormattedWeatherData from "./Services/WeatherService";
+import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  const [query, setQuery] = useState({ q: "tokyo" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const message = query.q ? query.q : "current location";
+
+      toast.info("Fetching weather for " + message);
+
+      await getFormattedWeatherData({ ...query, units }).then((data) => {
+        toast.success(
+          `Sucessfully fetched weather for ${data.name}, ${data.country}.`
+        );
+        setWeather(data);
+      });
+    };
+    fetchWeather();
+  }, [query, units]);
+
+  const formatBackground = () => {
+    if (!weather) return "from-cyan-700 to blue-700";
+    const threshold = units === "metric" ? 20 : 60;
+    if (weather.temp <= threshold) return "from-cyan-700 to blue-700";
+
+    return "from-yellow-700 to-orange-700";
+  };
+
+  return (
+    <div
+      className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400 ${formatBackground()}`}
+    >
+      <TopButtons setQuery={setQuery} />
+      <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
+      {weather && (
+        <div>
+          <TimeAndLocation weather={weather} />
+          <Details weather={weather} />
+          <Forecast title="Hourly Forecast" items={weather.hourly} />
+          <Forecast title="Daily Forecast" items={weather.daily} />
+        </div>
+      )}
+      <ToastContainer autoClose={5000} theme="colored" newestOnTop={true} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/TopButtons.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+function TopButtons({ setQuery }) {
+  const cities = [
+    {
+      id: 1,
+      title: "London",
+    },
+    {
+      id: 2,
+      title: "New York",
+    },
+    {
+      id: 3,
+      title: "Tokyo",
+    },
+    {
+      id: 4,
+      title: "Mumbai",
+    },
+    {
+      id: 5,
+      title: "Sydney",
+    },
+  ];
+  return (
+    <div className="flex items-center justify-around my-6">
+      {cities.map((city) => (
+        <button
+          key={city.id}
+          className="text-white text-lg font-medium"
+          onClick={() => setQuery({ q: city.title })}
+        >
+          {city.title}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default TopButtons;
